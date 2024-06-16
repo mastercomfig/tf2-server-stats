2fab6dbf3a855b46f1fbda8ca39788e6adc19c01
mastercoms <mastercoms@tuta.io>
2024-06-16 05:02:14 -0400

    finish ping handling

--- tf2_quickplay/app.py
+++ tf2_quickplay/app.py
@@ -1,26 +1,55 @@
 import asyncio
 import datetime
 import json
+import math
 import os
 import random
 import sys
+import tarfile
 import time
 import traceback
+import urllib.request
 from collections.abc import Callable
 from pathlib import Path
 
+import a2s
 import aiohttp
+import geoip2
+import geoip2.database
+import geopy.distance
 import tinydb
+import ujson
 import vdf
 
 STEAM_API_KEY = os.getenv("STEAM_API_KEY")
 if not STEAM_API_KEY:
     print("Need to pass in STEAM_API_KEY")
     sys.exit(1)
+COMFIG_API_KEY = os.getenv("COMFIG_API_KEY")
+if not COMFIG_API_KEY:
+    print("Need to pass in COMFIG_API_KEY")
+    sys.exit(1)
+GEOIP_KEY = os.getenv("GEOIP_KEY")
+if not GEOIP_KEY:
+    print("Need to pass in GEOIP_KEY")
+    sys.exit(1)
 STEAM_API_PARAM = {"key": STEAM_API_KEY}
-QUERY_INTERVAL = 4
+QUERY_INTERVAL = 60
 QUERY_FILTER = r"\appid\440\gamedir\tf\secure\1\dedicated\1\full\1\ngametype\hidden,friendlyfire,highlander,noquickplay,trade,dmgspread,mvm,pve\steamblocking\1\nor\1\white\1"
 QUERY_LIMIT = "20000"
+
+CONTINENTS = {
+    0: set(["NA"]),
+    1: set(["NA"]),
+    2: set(["SA"]),
+    3: set(["EU"]),
+    4: set(["AS"]),
+    5: set(["OC"]),
+    6: set(["AS", "EU"]),
+    7: set(["AF"]),
+}
+
+DEBUG = False
 
 OVERVIEW_INTERVAL = 300
 
@@ -31,7 +60,7 @@ APP_NAME = "tf"
 APP_FULL_NAME = "Team Fortress"
 
 MIN_PLAYER_CAP = 18
-MAX_PLAYER_CAP = 100
+MAX_PLAYER_CAP = 101
 FULL_PLAYERS = 33
 
 SERVER_HEADROOM = 1
@@ -122,6 +151,10 @@ def chaos(max=300) -> float:
     return random.uniform(0, max)
 
 
+def shuffle(val, pct=0.1) -> float:
+    return val + random.normalvariate(0, math.sqrt(val * pct))
+
+
 last_overview_resp = None
 next_overview_resp_time = 0
 last_items_game_resp = None
@@ -210,7 +243,11 @@ def score_server(humans: int, bots: int, max_players: int) -> float:
 
 
 async def query_runner(
-    api_session: aiohttp.ClientSession, cdn_session: aiohttp.ClientSession
+    geoasn: geoip2.database.Reader,
+    geoip: geoip2.database.Reader,
+    api_session: aiohttp.ClientSession,
+    cdn_session: aiohttp.ClientSession,
+    comfig_session: aiohttp.ClientSession,
 ):
     server_params = {"key": STEAM_API_KEY, "limit": QUERY_LIMIT, "filter": QUERY_FILTER}
     gamemodes: dict[str, set[str]] = {}
@@ -220,6 +257,15 @@ async def query_runner(
     banned_name_search = get_value("names", default=[], table=ban_table)
     banned_tags = set(get_value("tags", default=[], table=ban_table))
     items_game, updated, server_version = await req_items_game(api_session, cdn_session)
+    my_ip = "127.0.0.1"
+    async with aiohttp.ClientSession("https://api.ipify.org") as ip_session:
+        async with ip_session.get("/?format=json") as resp:
+            body = await resp.json()
+            my_ip = body["ip"]
+    my_city = geoip.city(my_ip)
+    my_lon = my_city.location.longitude
+    my_lat = my_city.location.latitude
+    my_point = (my_lat, my_lon)
     try:
         if items_game:
             if updated:
@@ -278,73 +324,137 @@ async def query_runner(
                 "/IGameServersService/GetServerList/v1/",
                 params=server_params,
             ) as resp:
-                players = 0
                 body = await resp.json(encoding="latin-1")
                 pending_servers = body["response"]["servers"]
-                new_servers = []
-                for server in pending_servers:
+
+                async def calc_server(server):
                     addr = server["addr"]
                     # skip servers with SDR
                     if addr.startswith("169.254"):
-                        continue
+                        if DEBUG:
+                            return {
+                                "score": -999,
+                                "removal": "sdr",
+                                "name": server["name"],
+                            }
+                        else:
+                            return None
                     # check for steam ID
                     steamid = server["steamid"]
                     if not steamid:
-                        continue
+                        if DEBUG:
+                            return {"score": -999, "removal": "nosteam"}
+                        else:
+                            return None
                     # not tf, leave
                     if server["appid"] != APP_ID:
-                        continue
+                        if DEBUG:
+                            return {"score": -999, "removal": "noappid"}
+                        else:
+                            return None
                     if server["gamedir"] != APP_NAME:
-                        continue
+                        if DEBUG:
+                            return {"score": -999, "removal": "nogamedir"}
+                        else:
+                            return None
                     if server["product"] != APP_NAME:
-                        continue
+                        if DEBUG:
+                            return {"score": -999, "removal": "noprod"}
+                        else:
+                            return None
                     # check max players
                     max_players = server["max_players"]
                     if max_players < MIN_PLAYER_CAP:
                         # not enough max_players
-                        continue
+                        if DEBUG:
+                            return {"score": -999, "removal": "<18"}
+                        else:
+                            return None
                     if max_players > MAX_PLAYER_CAP:
                         # too much max_players
-                        continue
+                        if DEBUG:
+                            return {"score": -999, "removal": ">101"}
+                        else:
+                            return None
                     num_players = server["players"]
                     if num_players >= max_players:
                         # lying about players
-                        continue
+                        if DEBUG:
+                            return {"score": -999, "removal": "playercaplie"}
+                        else:
+                            return None
                     # check if out of date
                     if int(server["version"]) < server_version:
-                        continue
+                        if DEBUG:
+                            return {"score": -999, "removal": "outofdate"}
+                        else:
+                            return None
                     # check if it's a casual map
                     map = server["map"]
                     if map not in map_gamemode:
-                        continue
+                        if DEBUG:
+                            return {"score": -999, "removal": "badmap", "map": map}
+                        else:
+                            return None
                     # check for ban
                     if server["steamid"] in banned_ids:
-                        continue
-                    ip = addr.split(":")[0]
+                        if DEBUG:
+                            return {"score": -999, "removal": "steamban"}
+                        else:
+                            return None
+                    ip, port = addr.split(":")
                     if ip in banned_ips:
-                        continue
+                        if DEBUG:
+                            return {"score": -999, "removal": "ipban"}
+                        else:
+                            return None
                     # check for gametype
                     gametype = set(server["gametype"].lower().split(","))
                     # is lying about max players?
-                    if max_players > 25 and "increased_maxplayers" not in gametype:
-                        continue
-                    if max_players <= 25 and "increased_maxplayers" in gametype:
-                        continue
+                    if max_players > 24 and "increased_maxplayers" not in gametype:
+                        if DEBUG:
+                            return {"score": -999, "removal": "-maxplayers"}
+                        else:
+                            return None
+                    if max_players <= 24 and "increased_maxplayers" in gametype:
+                        if DEBUG:
+                            return {"score": -999, "removal": "+maxplayers"}
+                        else:
+                            return None
                     # is it any of the gamemodes we want?
                     found_valid_gametype = (
                         len(gametype.intersection(ANY_VALID_TAGS)) > 0
                     )
-                    # is it the gamemode we want?
-                    expected_gamemode = map_gamemode[map]
-                    if expected_gamemode not in gametype:
-                        continue
-                    # check for tag errors
-                    if found_valid_gametype:
-                        found_valid_gametype = (
-                            len(gametype.intersection(banned_tags)) < 1
-                        )
                     if not found_valid_gametype:
-                        continue
+                        if DEBUG:
+                            return {
+                                "score": -999,
+                                "removal": "nogametype",
+                                "gametype": list(gametype),
+                                "name": server["name"],
+                            }
+                        else:
+                            return None
+                    # is it the gamemode we want?
+                    expected_gamemode = GAMEMODE_TO_TAG.get(map_gamemode[map])
+                    if expected_gamemode and expected_gamemode not in gametype:
+                        if DEBUG:
+                            return {
+                                "score": -999,
+                                "removal": "unexpectedtag",
+                                "map": map,
+                                "expected": expected_gamemode,
+                                "gametype": list(gametype),
+                            }
+                        else:
+                            return None
+                    # check for tag errors
+                    found_valid_gametype = len(gametype.intersection(banned_tags)) < 1
+                    if not found_valid_gametype:
+                        if DEBUG:
+                            return {"score": -999, "removal": "badgametype"}
+                        else:
+                            return None
                     # check for name errors
                     name = server["name"]
                     lower_name = name.lower()
@@ -353,8 +463,10 @@ async def query_runner(
                         if invalid in lower_name:
                             bad_name = True
                     if bad_name:
-                        continue
-                    players += num_players
+                        if DEBUG:
+                            return {"score": -999, "removal": "badname"}
+                        else:
+                            return None
                     bots = server["bots"]
                     rep = get_value(steamid, table=rep_table)
                     if rep is None:
@@ -362,39 +474,133 @@ async def query_runner(
                     score = rep + 6
                     score += score_server(num_players, bots, max_players)
                     if score < 0.1:
-                        continue
-                    new_servers.append(
-                        {
-                            "addr": addr,
-                            "steamid": steamid,
-                            "name": name,
-                            "region": server["region"],
-                            "players": num_players,
-                            "max_players": max_players,
-                            "bots": bots,
-                            "map": map,
-                            "gametype": list(gametype),
-                            "score": score,
-                        }
-                    )
-                print(players)
-                print(len(new_servers))
+                        if DEBUG:
+                            return {
+                                "score": -999,
+                                "removal": "lowscore",
+                                "score": score,
+                                "players": num_players,
+                                "bots": bots,
+                                "max": max_players,
+                            }
+                        else:
+                            return None
+                    try:
+                        server_query = await a2s.ainfo((ip, port))
+                    except:
+                        if DEBUG:
+                            return {"score": 0, "removal": "timeout"}
+                        else:
+                            return None
+                    if server_query.password_protected:
+                        if DEBUG:
+                            return {"score": 0, "removal": "pass"}
+                        else:
+                            return None
+                    if server_query.game != APP_FULL_NAME:
+                        if DEBUG:
+                            return {
+                                "score": -999,
+                                "removal": "incorrectgame",
+                                "name": name,
+                                "game": server_query.game,
+                            }
+                        else:
+                            return None
+                    # shift the scores around a little bit so we get some variance in sorting
+                    score = shuffle(score, pct=0.008)
+                    # calculate ping score
+                    ping = server_query.ping * 1000
+                    city = geoip.city(ip)
+                    country = city.country.iso_code
+                    continent = city.continent.code
+                    server_region = server["region"]
+                    # TODO: do something with non-matching regions
+                    lon = city.location.longitude
+                    lat = city.location.latitude
+                    point = (lat, lon)
+                    # asn = geoasn.asn(ip)
+                    # aso = asn.autonomous_system_organization
+                    # TODO: do something with malicious ASN usage for fake pings
+                    dist = geopy.distance.distance(my_point, point).km
+                    ideal = dist / 125
+                    overhead = max(ping - ideal - 2, 1)
+                    return {
+                        "addr": addr,
+                        "steamid": steamid,
+                        "name": name,
+                        # "region": server_region,
+                        # "continent": continent,
+                        # "country": country,
+                        "players": num_players,
+                        "max_players": max_players,
+                        "bots": bots,
+                        "map": map,
+                        "gametype": list(gametype),
+                        "score": score,
+                        "point": [lon, lat],
+                        "ping": overhead,
+                    }
+
+                server_infos = await asyncio.gather(
+                    *[calc_server(server) for server in pending_servers]
+                )
+                new_servers = [server for server in server_infos if server]
                 new_servers.sort(key=get_score, reverse=True)
-                # TODO
-                # query A2S for all servers
-                # post servers to API
+                with open("servers.json", "w") as fp:
+                    json.dump(new_servers, fp)
+                async with comfig_session.post(
+                    "/api/quickplay/update",
+                    headers={"Authorization": f"Bearer {COMFIG_API_KEY}"},
+                    json={"servers": new_servers},
+                ) as api_resp:
+                    print(await api_resp.text())
+                print(len(new_servers))
     except Exception:
         traceback.print_exc()
 
-    await asyncio.sleep(QUERY_INTERVAL + chaos(2))
+    await asyncio.sleep(QUERY_INTERVAL + chaos(0.1))
+
+
+def handle_geoip(geoipDb, edition):
+    if geoipDb.exists():
+        diff = utcnow().timestamp() - geoipDb.stat().st_mtime
+        if diff / 24 / 3600 / 1000 <= 30:
+            return True
+    archive_name = f"./{edition}.tar.gz"
+    urllib.request.urlretrieve(
+        f"https://download.maxmind.com/app/geoip_download?edition_id={edition}&license_key={GEOIP_KEY}&suffix=tar.gz",
+        archive_name,
+    )
+    with tarfile.open(archive_name) as tar:
+        members = tar.getmembers()
+        for member in members:
+            if member.name.endswith(".mmdb"):
+                with tar.extractfile(member) as db:
+                    with open(geoipDb, "wb") as out:
+                        out.write(db.read())
+                break
+    return True
 
 
 async def main():
-    async with aiohttp.ClientSession(
-        base_url="https://api.steampowered.com"
-    ) as api_session:
-        async with aiohttp.ClientSession(base_url=CDN_BASE_URL) as cdn_session:
-            await query_runner(api_session, cdn_session)
+    geoipDb = Path(f"./GeoIP2-City.mmdb")
+    handle_geoip(geoipDb, "GeoLite2-City")
+    geoAsnDb = Path(f"./GeoIP2-ASN.mmdb")
+    handle_geoip(geoAsnDb, "GeoLite2-ASN")
+    with geoip2.database.Reader(geoAsnDb) as geoasn:
+        with geoip2.database.Reader(geoipDb) as geoip:
+            async with aiohttp.ClientSession(
+                base_url="https://api.steampowered.com"
+            ) as api_session:
+                async with aiohttp.ClientSession(base_url=CDN_BASE_URL) as cdn_session:
+                    async with aiohttp.ClientSession(
+                        base_url="https://worker.comfig.app",
+                        json_serialize=ujson.dumps,
+                    ) as comfig_session:
+                        await query_runner(
+                            geoasn, geoip, api_session, cdn_session, comfig_session
+                        )
 
 
 def start():
